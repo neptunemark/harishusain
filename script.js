f65a9a3753cafd19b510bcacadfdc39747a0658c
Your Name <new.anaspakattippara@gmail.com>
2024-01-13 12:58:04 +0530

    haris

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const person = {
-    profile_pic: "./image/shahink.jpeg",
-    profile_logo: "./image/DBI.jpg",
+    profile_pic: "./image/haris.jpg",
+    profile_logo: "./image/aislogo.jpeg",
 
-    Name: "Muhammed Shahin",
-    designation: "",
-    des_link_text: "",
+    Name: "Haris Husain",
+    designation: "Managing Director",
+    des_link_text: "@ Aiswarya Home",
     des_link: "",
-    contact: "./contact/Shahin.vcf",
+    contact: "./contact/harishusain.vcf",
 };
 
 const contact ={
-    contact_no: "97336756788",
-    whatsapp: "+919544105950;",
-    location: "https://maps.app.goo.gl/SWvHRAoQzd76kZDfA?g_st=ic",
-    email: "mohdshahin.pk@gmail.com",
+    contact_no: "919946467657",
+    whatsapp: "+917034499661;",
+    location: "https://maps.app.goo.gl/jRuWzxoj3TQjv2DHA",
+    email: "aiswaryafurnitureclt@gmail.com",
 };
 
 const profilelogo=[
@@ -27,22 +27,22 @@ const profilelogo=[
 const links =[
     facebook={
     visibil: true,
-    link:"https://www.facebook.com/shahinpk?mibextid=LQQJ4d"
+    link:"https://www.facebook.com/aiswaryafunituregroup"
   },
     instagram={
     visibil: true,
-    link:"https://www.instagram.com/shahinpkt?igshid=OGQ5ZDc2ODk2ZA=="
+    link:"https://www.instagram.com/aiswarya.home/"
   },
     skype={
-    visibil: true,
+    visibil: false,
     link:"https://join.skype.com/invite/drP1omgbrHHV"
   },
     web={
-    visibil: false,
-    link:"https://neptunemark.com/"
+    visibil: true,
+    link:"https://aiswaryafurniture.com/"
   },
     linkedin={
-    visibil: true,
+    visibil: false,
     link:"https://www.linkedin.com/in/muhammed-shahin-aa2236237?utm_source=share&amp;utm_campaign=share_via&amp;utm_content=profile&amp;utm_medium=ios_app"
   },
     behance={
@@ -50,23 +50,23 @@ const links =[
     link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
   },
     x={
-    visibil: true,
+    visibil: false,
     link:"https://x.com/shahinpkt/"
   },
     mail={
     visibil: true,
-    link:"mohdshahin.pk@gmail.com"
+    link:"aiswaryafurnitureclt@gmail.com"
   },
     google={
-    visibil: false,
-    link:"https://navigotrips.com/"
+    visibil: true,
+    link:"https://maps.app.goo.gl/jRuWzxoj3TQjv2DHA"
   },
     telegram={
     visibil: false,
     link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
   },
     youtube={
-    visibil: false,
+    visibil: true,
     link:"https://maps.app.goo.gl/zMguVmn3NbNHkdAb7"
   },
     pinterest={
